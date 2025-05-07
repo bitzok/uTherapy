cecb9b5db14f962b07e055961ae68dc9b8394bea
bitzok <santacruzedward@hotmail.com>
2025-05-07 10:29:00 -0500

    backup antes de actualizar a expo SDK 53

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from api.routes import auth, modelCall
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=[os.getenv("URL_DEV")],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
